1eea564517c8cf3af1831338555475bfebde4e8c
nsc1234 <nsc9012@gmail.com>
2019-10-16 18:05:50 -0600

    Add Kotlin support to project and convert BitLog to Kotlin

--- yospace/src/main/java/com/bitmovin/player/integration/yospace/YospaceUtil.java
+++ yospace/src/main/java/com/bitmovin/player/integration/yospace/YospaceUtil.java
@@ -73,7 +73,7 @@ public class YospaceUtil {
                     continue;
                 }
 
-                BitLog.d("Key: " + key + " Value: " + value);
+                BitLog.INSTANCE.d("Key: " + key + " Value: " + value);
 
                 if (key.equals("YMID")) {
                     ymid = value;

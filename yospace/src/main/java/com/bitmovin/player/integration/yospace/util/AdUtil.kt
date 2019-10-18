88238431500be1a12fea54dfdd604457996b89ac
nsc1234 <nsc9012@gmail.com>
2019-10-18 14:28:31 -0600

    Create koltin adtimeline

--- yospace/src/main/java/com/bitmovin/player/integration/yospace/util/AdUtil.kt
+++ yospace/src/main/java/com/bitmovin/player/integration/yospace/util/AdUtil.kt
@@ -2,7 +2,9 @@ package com.bitmovin.player.integration.yospace.util
 
 import com.bitmovin.player.config.advertising.AdSourceType
 import com.bitmovin.player.integration.yospace.Ad
+import com.bitmovin.player.integration.yospace.AdBreak
 import com.bitmovin.player.integration.yospace.YospaceAdStartedEvent
+import com.yospace.android.hls.analytic.advert.AdBreak as YospaceAdBreak
 import com.yospace.android.hls.analytic.advert.Advert as YospaceAd
 
 fun Ad.toAdStartedEvent(): YospaceAdStartedEvent =
@@ -10,6 +12,35 @@ fun Ad.toAdStartedEvent(): YospaceAdStartedEvent =
 
 fun buildEmptyAdStartedEvent(): YospaceAdStartedEvent =
     YospaceAdStartedEvent(AdSourceType.UNKNOWN, "", 0, 0.0, 0.0, "0", 0.0, true)
+
+fun List<YospaceAdBreak>.toAdBreaks(): List<AdBreak> {
+    var offset = 0.0
+    return map { yospaceAdBreak ->
+        val adBreak = AdBreak(
+            "unknown",
+            (yospaceAdBreak.startMillis - offset) / 1000,
+            yospaceAdBreak.duration / 1000.0,
+            yospaceAdBreak.startMillis / 1000.0,
+            (yospaceAdBreak.startMillis + yospaceAdBreak.duration) / 1000.0
+        )
+        offset += yospaceAdBreak.duration
+        yospaceAdBreak.adverts.forEach { yospaceAd ->
+            val ad = Ad(
+                yospaceAd.identifier,
+                adBreak.relativeStart / 1000,
+                yospaceAd.duration / 1000.0,
+                yospaceAd.startMillis / 1000.0,
+                (yospaceAd.startMillis + yospaceAd.duration) / 1000.0,
+                yospaceAd.sequence,
+                yospaceAd.getAdClickThroughUrl(),
+                yospaceAd.hasLinearInteractiveUnit(),
+                yospaceAd.isTrueX()
+            )
+            adBreak.appendAd(ad)
+        }
+        adBreak
+    }
+}
 
 fun YospaceAd.toAdStartedEvent(): YospaceAdStartedEvent =
     YospaceAdStartedEvent(AdSourceType.UNKNOWN, getAdClickThroughUrl(), sequence, duration / 1000.0, startMillis / 1000.0, "position", 0.0, isTrueX())

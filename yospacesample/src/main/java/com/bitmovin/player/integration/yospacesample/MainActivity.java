b473cdad14f96412d6808225813296891681930f
nsc1234 <nsc9012@gmail.com>
2019-10-21 09:23:58 -0600

    Fix logs, rename trueX, tidy code

--- yospacesample/src/main/java/com/bitmovin/player/integration/yospacesample/MainActivity.java
+++ yospacesample/src/main/java/com/bitmovin/player/integration/yospacesample/MainActivity.java
@@ -41,7 +41,7 @@ import com.bitmovin.player.integration.yospace.Ad;
 import com.bitmovin.player.integration.yospace.BitmovinYospacePlayer;
 import com.bitmovin.player.integration.yospace.YospaceAdStartedEvent;
 import com.bitmovin.player.integration.yospace.YospaceAssetType;
-import com.bitmovin.player.integration.yospace.config.TrueXConfiguration;
+import com.bitmovin.player.integration.yospace.config.TruexConfiguration;
 import com.bitmovin.player.integration.yospace.config.YospaceConfiguration;
 import com.bitmovin.player.integration.yospace.config.YospaceSourceConfiguration;
 
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BitmovinYospacePlayer bitmovinYospacePlayer;
 
     private String currentClickThroughUrl;
-    private TrueXConfiguration trueXConfiguration;
+    private TruexConfiguration trueXConfiguration;
 
     private static final String TAG = "MainActivity";
 
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         PlayerConfiguration playerConfiguration = new PlayerConfiguration();
         YospaceConfiguration yospaceConfiguration = new YospaceConfiguration(null, 25_000, 25_000, 25_000, true);
-        trueXConfiguration = new TrueXConfiguration(bitmovinPlayerView, "turner_bm_ys_tester_001", "qa-get.truex.com/07d5fe7cc7f9b5ab86112433cf0a83b6fb41b092/vast/config?asnw=&cpx_url=&dimension_2=0&flag=%2Bamcb%2Bemcr%2Bslcb%2Bvicb%2Baeti-exvt&fw_key_values=&metr=0&network_user_id=turner_bm_ys_tester_001&prof=g_as3_truex&ptgt=a&pvrn=&resp=vmap1&slid=fw_truex&ssnw=&stream_position=midroll&vdur=&vprn=");
+        trueXConfiguration = new TruexConfiguration(bitmovinPlayerView, "turner_bm_ys_tester_001", "qa-get.truex.com/07d5fe7cc7f9b5ab86112433cf0a83b6fb41b092/vast/config?asnw=&cpx_url=&dimension_2=0&flag=%2Bamcb%2Bemcr%2Bslcb%2Bvicb%2Baeti-exvt&fw_key_values=&metr=0&network_user_id=turner_bm_ys_tester_001&prof=g_as3_truex&ptgt=a&pvrn=&resp=vmap1&slid=fw_truex&ssnw=&stream_position=midroll&vdur=&vprn=");
 
         bitmovinYospacePlayer = new BitmovinYospacePlayer(getApplicationContext(), playerConfiguration, yospaceConfiguration);
         bitmovinYospacePlayer.getConfig().getPlaybackConfiguration().setAutoplayEnabled(true);

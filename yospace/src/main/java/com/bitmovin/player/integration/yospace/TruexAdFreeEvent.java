536475f9c1ce6bd7fc72e5793bae07e021da35f4
aneurinc <nsc9012@gmail.com>
2020-02-13 16:06:34 -0700

    Convert truexadfreeevent to kt

--- yospace/src/main/java/com/bitmovin/player/integration/yospace/TruexAdFreeEvent.java
+++
@@ -1,7 +0,0 @@
-package com.bitmovin.player.integration.yospace;
-
-import com.bitmovin.player.api.event.data.BitmovinPlayerEvent;
-
-public class TruexAdFreeEvent extends BitmovinPlayerEvent {
-
-}

758a2e4786e7f2d3c9c4d69d11a1acab8d563e7b
aneurinc <nsc9012@gmail.com>
2020-03-18 12:15:50 -0600

    Use ad id instead of identifier

--- yospace/src/main/java/com/bitmovin/player/integration/yospace/util/AdvertExt.kt
+++ yospace/src/main/java/com/bitmovin/player/integration/yospace/util/AdvertExt.kt
@@ -11,7 +11,7 @@ fun YsAd.adMimeType(): String = linearCreative?.interactiveUnit?.mimeType ?: ""
 fun YsAd.isTruex(): Boolean = adSystem?.adSystemType == "trueX"
 
 fun YsAd.toAd(adBreakRelativeStart: Double): Ad = Ad(
-    id = identifier,
+    id = id,
     relativeStart = adBreakRelativeStart / 1000,
     duration = duration / 1000.0,
     absoluteStart = startMillis / 1000.0,

3d3226bc47dfbe1b6c5fd98664297c9836a3de72
aneurinc <aneurin@bitmovin.com>
2020-11-20 17:05:08 -0700

    Fix function signature

--- yospace/src/main/java/com/bitmovin/player/integration/yospace/BitmovinYospacePlayer.kt
+++ yospace/src/main/java/com/bitmovin/player/integration/yospace/BitmovinYospacePlayer.kt
@@ -362,7 +362,7 @@ open class BitmovinYospacePlayer(
 
     fun onCompanionClickThrough(companionId: String) = yospaceSession?.onCompanionClickThrough(companionId)
 
-    fun onCompanionRenderd(companionId: String) = yospaceSession?.onCompanionEvent("creativeView", companionId)
+    fun onCompanionRendered(companionId: String) = yospaceSession?.onCompanionEvent("creativeView", companionId)
 
     ///////////////////////////////////////////////////////////////
     // YoSpace session
